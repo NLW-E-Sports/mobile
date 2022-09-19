f51ec78afde592936ab6fc0ab1ec1fee7719d113
Rafhael Mallorga <rafhaelmallorga@gmail.com>
2022-09-19 12:17:54 -0300

    feat :sparkles: created axios api connection and linked with API

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,14 +1,20 @@
 import { View, Image, FlatList } from "react-native";
+import { useEffect, useState } from "react";
 
 import logoImg from "../../assets/logo-nlw-esports.png";
-import { GameCard } from "../../components/GameCard";
+import { GameCard, GameCardProps } from "../../components/GameCard";
 import { Heading } from "../../components/Header";
 
-import { GAMES } from "../../utils/games";
-
 import { styles } from "./styles";
+import api from "../../api";
 
 export function Home() {
+  const [games, setGames] = useState<GameCardProps[]>([]);
+
+  useEffect(() => {
+    api.get("/games").then((res) => setGames(res.data));
+  }, []);
+
   return (
     <View style={styles.container}>
       <Image source={logoImg} style={styles.logo} />
@@ -18,7 +24,7 @@ export function Home() {
       />
       <FlatList
         contentContainerStyle={styles.contentList}
-        data={GAMES}
+        data={games}
         keyExtractor={(item) => item.id}
         renderItem={({ item }) => <GameCard data={item} />}
         showsHorizontalScrollIndicator={false}
