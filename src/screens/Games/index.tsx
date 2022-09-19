da4f5a07ac47be77e48b7b69bdd80817bb738397
Rafhael Mallorga <rafhaelmallorga@gmail.com>
2022-09-19 15:07:04 -0300

    feat :sparkles: created page game and navigation relations

---
+++ src/screens/Games/index.tsx
@@ -0,0 +1,50 @@
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useRoute, useNavigation } from "@react-navigation/native";
+import { Background } from "../../components/Background";
+import { Entypo } from "@expo/vector-icons";
+
+import logoImg from "../../assets/logo-nlw-esports.png";
+
+import { GameParams } from "../../@types/navigation";
+
+import { styles } from "./styles";
+import { Image, TouchableOpacity, View } from "react-native";
+import { THEME } from "../../theme";
+import { Heading } from "../../components/Header";
+
+export const Game = () => {
+  const route = useRoute();
+  const game = route.params as GameParams;
+
+  const navigation = useNavigation();
+
+  const handleGoBack = () => {
+    navigation.goBack();
+  };
+
+  return (
+    <Background>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.header}>
+          <TouchableOpacity onPress={handleGoBack}>
+            <Entypo
+              name="chevron-thin-left"
+              color={THEME.COLORS.CAPTION_300}
+              size={20}
+            />
+          </TouchableOpacity>
+          <Image source={logoImg} style={styles.logo} />
+          <View style={styles.right} />
+        </View>
+
+        <Image
+          source={{ uri: game.bannerUrl }}
+          style={styles.cover}
+          resizeMode="cover"
+        />
+
+        <Heading title={game.title} subtitle="Conecte-se e comece a jogar!" />
+      </SafeAreaView>
+    </Background>
+  );
+};
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,35 +1,49 @@
-import { View, Image, FlatList } from "react-native";
+import { Image, FlatList } from "react-native";
 import { useEffect, useState } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 import logoImg from "../../assets/logo-nlw-esports.png";
 import { GameCard, GameCardProps } from "../../components/GameCard";
 import { Heading } from "../../components/Header";
 
+import { useNavigation } from "@react-navigation/native";
+
 import { styles } from "./styles";
 import api from "../../api";
+import { Background } from "../../components/Background";
 
 export function Home() {
   const [games, setGames] = useState<GameCardProps[]>([]);
+
+  const navigation = useNavigation();
+
+  const handleOpenGame = ({ id, title, bannerUrl }: GameCardProps) => {
+    navigation.navigate("game", { id, title, bannerUrl });
+  };
 
   useEffect(() => {
     api.get("/games").then((res) => setGames(res.data));
   }, []);
 
   return (
-    <View style={styles.container}>
-      <Image source={logoImg} style={styles.logo} />
-      <Heading
-        title="Encontre seu duo"
-        subtitle="Selecione o game que deseja jogar"
-      />
-      <FlatList
-        contentContainerStyle={styles.contentList}
-        data={games}
-        keyExtractor={(item) => item.id}
-        renderItem={({ item }) => <GameCard data={item} />}
-        showsHorizontalScrollIndicator={false}
-        horizontal
-      />
-    </View>
+    <Background>
+      <SafeAreaView style={styles.container}>
+        <Image source={logoImg} style={styles.logo} />
+        <Heading
+          title="Encontre seu duo"
+          subtitle="Selecione o game que deseja jogar"
+        />
+        <FlatList
+          contentContainerStyle={styles.contentList}
+          data={games}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => (
+            <GameCard data={item} onPress={() => handleOpenGame(item)} />
+          )}
+          showsHorizontalScrollIndicator={false}
+          horizontal
+        />
+      </SafeAreaView>
+    </Background>
   );
 }
