111ba8935ec2d9f86f5313b7ca0c7189646a6f6e
Rafhael Mallorga <rafhaelmallorga@gmail.com>
2022-09-19 16:56:03 -0300

    feat :sparkles: created ads section and modals

--- src/screens/Games/styles.ts
+++ src/screens/Games/styles.ts
@@ -1,8 +1,10 @@
 import {
+  Inter_100Thin,
   Inter_200ExtraLight,
   Inter_400Regular,
 } from "@expo-google-fonts/inter";
 import { StyleSheet } from "react-native";
+import { THEME } from "../../theme";
 
 export const styles = StyleSheet.create({
   container: {
@@ -30,5 +32,23 @@ export const styles = StyleSheet.create({
     height: 160,
     borderRadius: 8,
     marginTop: 32,
+  },
+  containerList: {
+    width: "100%",
+  },
+  contentList: {
+    paddingLeft: 32,
+    paddingRight: 64,
+    alignItems: "flex-start",
+  },
+  emptyListText: {
+    color: THEME.COLORS.CAPTION_300,
+    fontSize: THEME.FONT_SIZE.SM,
+    fontFamily: THEME.FONT_FAMILY.REGULAR,
+  },
+  emptyListContext: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
